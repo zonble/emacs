b7a1d486dfa9d3b884a9186aae4fa37290604ab9
zonble <zonble@gmail.com>
2012-12-02 16:00:42 +0800

    Updates settings.

--- defunkt/custom.el
+++ defunkt/custom.el
@@ -5,10 +5,10 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(ansi-color-names-vector ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
- '(cua-mode nil nil (cua-base))
+ '(cua-mode t nil (cua-base))
  '(custom-enabled-themes (quote (wheatgrass)))
  '(erc-modules (quote (autojoin button completion fill irccontrols match menu netsplit noncommands readonly ring scrolltobottom stamp track)))
- '(javascript-shell-command "johnson")
+ '(javascript-shell-command "jsc")
  '(js2-auto-indent-flag nil)
  '(js2-basic-offset 2)
  '(js2-bounce-indent-flag t)
@@ -18,10 +18,8 @@
  '(ruby-deep-arglist nil)
  '(ruby-deep-indent-paren nil)
  '(ruby-deep-indent-paren-style nil)
- '(show-paren-mode nil)
- '(standard-indent 2)
- '(tool-bar-mode nil)
- '(transient-mark-mode t))
+ '(standard-indent 4xs)
+ '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -32,3 +30,5 @@
  '(minibuffer-prompt ((((background dark)) (:foreground "cyan" :height 180))))
  '(mode-line ((t (:inherit aquamacs-variable-width :background "grey75" :foreground "black" :width normal))))
  '(text-mode-default ((t (:inherit autoface-default))) t))
+
+(set-face-attribute 'default nil :family "Monaco" :height 180 :weight 'normal)
