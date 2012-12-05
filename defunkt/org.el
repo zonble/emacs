d76704843b4409452e0796ab4a525140377f0de9
zonble <zonble@gmail.com>
2012-12-05 19:12:17 +0800

    Updates settings.

--- defunkt/org.el
+++ defunkt/org.el
@@ -7,12 +7,12 @@
 (set 'org-log-done t)
 (set 'org-directory "~/Dropbox/org")
 (set 'org-agenda-files (list "~/Dropbox/org/work.org"
-							 "~/Dropbox/org/week.org"
-							 "~/Dropbox/org/home.org"))
+                             "~/Dropbox/org/week.org"
+                             "~/Dropbox/org/home.org"))
 
 (add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
 (add-hook 'org-mode-hook 'turn-on-font-lock)
 (global-set-key "\C-cl" 'org-store-link)
 (global-set-key "\C-ca" 'org-agenda)
 (global-set-key "\C-cb" 'org-iswitchb)
-(global-set-key "\C-cy" 'org-insert-trac-ticket-number)
+(global-set-key "\C-ct" 'org-insert-trac-ticket-number)
--- zonble.el
+++ zonble.el
@@ -1,14 +1,23 @@
 ; Custom functions
 
+(defun osstatus (number)
+  "Convert osstatus"
+  (interactive "nOSStatus:")
+  (let* ((a (logand (lsh number -24) 255))
+         (b (logand (lsh number -16) 255))
+         (c (logand (lsh number -8) 255))
+         (d (logand (lsh number 0) 255)))
+    (message (format "Output: %c%c%c%c" a b c d))))
+
 (defun now ()
   "Insert string for the current time formatted like '2:34 PM'."
-  (interactive)						 ; permit invocation in minibuffer
+  (interactive)                      ; permit invocation in minibuffer
   (insert (format-time-string "%D %-I:%M %p")))
 
 (defun today ()
   "Insert string for today's date nicely formatted in American style,
 e.g. Sunday, September 17, 2000."
-  (interactive)						 ; permit invocation in minibuffer
+  (interactive)                      ; permit invocation in minibuffer
   (insert (format-time-string "%A, %B %e, %Y")))
 
 (setq inhibit-startup-message t)
